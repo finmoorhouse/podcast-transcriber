5518d3c0517470a2aa241223eb639bd6c2c6e598
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2024-10-15 15:23:11 +0100

    Updated email address to personal site

--- email_utils.py
+++ email_utils.py
@@ -15,10 +15,10 @@ def send_confirmation_email(recipient_email):
     mailgun_domain = os.getenv("MAILGUN_DOMAIN")
     mailgun_api_key = os.getenv("MAILGUN_API_KEY")
 
-    url = f"https://api.mailgun.net/v3/{mailgun_domain}/messages"
+    url = f"https://api.eu.mailgun.net/v3/{mailgun_domain}/messages"
 
     data = {
-        "from": f"Transcription Service <mailgun@{mailgun_domain}>",
+        "from": f"Transcription Service <noreply@{mailgun_domain}>",
         "to": [recipient_email],
         "subject": "Transcription Request Received",
         "text": "Your transcription request has been received and is being processed. We'll notify you when it's complete.",
@@ -53,16 +53,16 @@ def send_completion_email(recipient_email, transcript_path):
     mailgun_domain = os.getenv("MAILGUN_DOMAIN")
     mailgun_api_key = os.getenv("MAILGUN_API_KEY")
 
-    url = f"https://api.mailgun.net/v3/{mailgun_domain}/messages"
+    url = f"https://api.eu.mailgun.net/v3/{mailgun_domain}/messages"
 
     data = {
         "from": f"Transcription Service <mailgun@{mailgun_domain}>",
         "to": [recipient_email],
         "subject": "Transcription Request Complete",
         "text": "Your transcription is ready, and you should find it as an attachment.\n\nThank you for using Podcast Transcripter!",
-        "attachment": (transcript_path, open(transcript_path, "rb")),
+        #"attachment": (transcript_path, open(transcript_path, "rb")),
     }
-    files = [("attachment", ("transcript.md", open(transcript_path, "rb").read()))]
+    files = [("attachment", ("transcript.md", open(transcript_path, "rb")))]
     print(f"Sending email to {recipient_email}")
     print(f"Using Mailgun URL: {url}")
     print(f"Request data: {data}")
